b2ed44fbecc0c321ff4d59035563e2c1d7bdf9ab
wpghks <wpghks56@naver.com>
2023-07-26 16:04:40 +0900

    [init] project Init

---
+++ bin/shelf_core_example.dart
@@ -0,0 +1,5 @@
+import 'package:shelf_core_example/shelf_core_example.dart' as shelf_core_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shelf_core_example.calculate()}!');
+}
---
+++ test/shelf_core_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:shelf_core_example/shelf_core_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
